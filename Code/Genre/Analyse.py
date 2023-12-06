189507be41b625ec6368db4655451cb615d04dc3
B. Daniel <120137318+RKusanali@users.noreply.github.com>
2023-12-06 09:54:56 +0100

    Update Analyse.py with brisque

--- Code/Genre/Analyse.py
+++ Code/Genre/Analyse.py
@@ -2,6 +2,7 @@
 import json
 import os
 import cv2
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
@@ -37,6 +38,21 @@ class Analyse:
 
     def union_path(self):
         self.path_file = os.path.join(self.directory, self.json_file)
+
+    def calculate_scores(self, image_path):
+        img = cv.imread(image_path)
+        if img is None:
+            print(f"Impossible de lire l'image à partir du chemin : {image_path}")
+            return
+
+        grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        blurScore = cv.Laplacian(grey, cv.CV_64F).var()
+        score = cv.quality.QualityBRISQUE_compute(img, "./classifieurs/brisque_model_live.yml", "./classifieurs/brisque_range_live.yml")
+
+        #print(f' >> Blur Score: {blurScore}')
+        #print(f' >> BRISQUE Score: {score}')
+
+        return blurScore, score
 
     def update_or_create_entry(self, s, s2, s3 = str("Unknow"), s4 = str("Swap"), gan_parameters = None, option = 1):
 
